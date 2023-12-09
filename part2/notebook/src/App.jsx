8137c495a8ba8e68fd7a99286cc314f8091af1cc
mhnash <nasir.shuvo@tuni.fi>
2023-12-09 16:12:39 +0200

    json server get done

--- part2/notebook/src/App.jsx
+++ part2/notebook/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
+
+
 
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('A new note.....')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(()=>{
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  },[])
+  console.log('render', notes.length, 'notes')
+
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
   const addNote = (event) => {
